4c67d865b9f0cbd18273f4b1c1184c3c1a0c874e
HelenaTsehova <helnaatsehova@gmail.com>
2023-02-24 02:16:11 +0300

    added send document

--- src/Service/TelegramBotClient.php
+++ src/Service/TelegramBotClient.php
@@ -5,12 +5,15 @@ namespace Helenatsehova\TelegramBotClient\Service;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mime\Part\DataPart;
+use Symfony\Component\Mime\Part\Multipart\FormDataPart;
 
 class TelegramBotClient
 {
     private const API = 'https://api.telegram.org/bot';
     private const SEND_MESSAGE = 'sendMessage';
     private const GET_UPDATES = 'getUpdates';
+    private const SEND_DOCUMENT = 'sendDocument';
 
     private string $token;
     private HttpClientInterface $httpClient;
@@ -41,5 +44,30 @@ class TelegramBotClient
         );
 
         return $response->toArray();
+    }
+
+    public function sendDocument(string $chatId, string $filePath)
+    {
+        $formFields = [
+            'chat_id' => $chatId,
+            'document' => DataPart::fromPath($filePath),
+        ];
+        $formData = new FormDataPart($formFields);
+
+        $response = $this->httpClient->request(
+            Request::METHOD_POST,
+            $this->getUri(self::SEND_DOCUMENT),
+            [
+                'headers' => $formData->getPreparedHeaders()->toArray(),
+                'body' => $formData->bodyToIterable(),
+            ]
+        );
+
+        return $response->toArray();
+    }
+
+    private function getUri(string $telegramMethod): string
+    {
+        return self::API . $this->token . '/' . $telegramMethod;
     }
 }
